b0c30c67970568332fa2e31303c237fa76b46268
vadimkantor <info@kantor-online.de>
2021-01-02 23:11:31 +0100

    add initial scenario for performance tuning

--- performance-tuning/assets/startLoad.sh
+++ performance-tuning/assets/startLoad.sh
@@ -1,1 +1,1 @@
-docker run -d --network network.wildfly --interactive --tty --volume `pwd`:/jmeter egaillardon/jmeter --nongui --testfile test.jmx --logfile result.jtl
+docker run -d --interactive --tty --volume `pwd`:/jmeter egaillardon/jmeter --nongui --testfile test.jmx --logfile result.jtl
