87bedefda9ead7e4202d80122af5bdd59e0a97c7
vadimkantor <info@kantor-online.de>
2021-01-02 23:06:37 +0100

    add initial scenario for performance tuning

--- performance-tuning/assets/startLoad.sh
+++ performance-tuning/assets/startLoad.sh
@@ -1,2 +1,1 @@
-docker network connect network.wildfly wildfly
 docker run -d --network network.wildfly --interactive --tty --volume `pwd`:/jmeter egaillardon/jmeter --nongui --testfile test.jmx --logfile result.jtl
--- performance-tuning/setupEnv.sh
+++ performance-tuning/setupEnv.sh
@@ -2,4 +2,6 @@ ssh root@host01 "docker network create network.wildfly"
 echo "create network network.wildfly"
 ssh root@host01 "docker run -d -p 8080:8080 --network network.wildfly --name wildfly jboss/wildfly"
 echo "starting wildfly server"
+ssh root@host01 "docker network connect network.wildfly wildfly"
+echo "network connected"
 echo "done" >> /root/katacoda-finished
