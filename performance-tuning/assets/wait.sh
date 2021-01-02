7f024cc2e902de6fa78ea2f442a494ee6e4d4083
vadimkantor <info@kantor-online.de>
2021-01-02 13:32:27 +0100

    add initial scenario for performance tuning

--- performance-tuning/assets/wait.sh
+++ performance-tuning/assets/wait.sh
@@ -22,22 +22,6 @@ show_progress()
   printf "    \b\b\b\b"
   echo ""
   echo "Started"
-  echo -n "Configuring"
-  while true; do
-    sudo grep -i "done" /root/katacoda-background-finished &> /dev/null
-    if [[ "$?" -ne 0 ]]; then
-      temp="${spinstr#?}"
-      printf " [%c]  " "${spinstr}"
-      spinstr=${temp}${spinstr%"${temp}"}
-      sleep "${delay}"
-      printf "\b\b\b\b\b\b"
-    else
-      break
-    fi
-  done
-  printf "    \b\b\b\b"
-  echo ""
-  echo "Configured"
 }
 
 show_progress
