c7993607e77fa3662fd08b3c7ca41a21dcce24e9
vadimkantor <info@kantor-online.de>
2021-01-03 21:58:14 +0100

    add initial scenario for performance tuning

--- performance-tuning/startWildFly.sh
+++ performance-tuning/startWildFly.sh
@@ -1,3 +1,3 @@
-ssh root@host01 "docker run -d -p 8080:8080 --name wildfly -e JAVA_OPTS=\"-XX:+UseG1GC -Xms512m -Xmx512m -Xlog:gc=trace:file=gc.log -Xlog:all=trace\" jboss/wildfly"
+ssh root@host01 "docker run -d -p 8080:8080 --name wildfly -e JAVA_OPTS=\"-XX:+UseG1GC -Xms512m -Xmx512m -Xlog:all=trace:gc.log\" jboss/wildfly"
 
 echo "done" >> /root/katacoda-finished
